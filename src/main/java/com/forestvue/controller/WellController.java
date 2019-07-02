b960c661b4834bb995c20db8e01a480a5cc3e2d2
Choi Seungeon <chou610@naver.com>
2019-07-02 23:53:37 +0900

    [Added] /draw : get water from well

---
+++ src/main/java/com/forestvue/controller/WellController.java
@@ -0,0 +1,42 @@
+package com.forestvue.controller;
+
+import com.forestvue.domain.UserDrawInfo;
+import com.forestvue.service.WellService;
+import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@Log4j
+public class WellController {
+
+    @Autowired
+    WellService wellService;
+
+    @RequestMapping(value="/")
+    public String index(Model model){
+        model.addAttribute("test", "1234");
+        return "index";
+    }
+    @PostMapping(value="/draw", produces = "application/json")
+    @ResponseBody
+    public Object draw(@RequestBody UserDrawInfo info){
+        wellService.draw(info);
+        Map<String, String> m = new HashMap<>();
+        m.put("message", "success");
+        return m;
+    }
+    @RequestMapping(value="/json", produces = "application/json")
+    @ResponseBody
+    public Object json(){
+        Map<String, String> m = new HashMap<>();
+        m.put("test", "1234");
+        return m;
+    }
+}
+
