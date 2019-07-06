bdce9b9e5a5bbdd001c2ed080d7876e665454cbb
Choi Seungeon <chou610@naver.com>
2019-07-06 22:08:32 +0900

    [Update] single user action test

--- src/test/java/cocurrency/UserDrawTest.java
+++ src/test/java/cocurrency/UserDrawTest.java
@@ -2,14 +2,17 @@ package cocurrency;
 
 import com.forestvue.config.RootConfig;
 import com.forestvue.config.ServletConfig;
+import com.forestvue.domain.UserDrawInfo;
 import com.forestvue.domain.UserScoreVO;
 import com.forestvue.service.WellService;
+import com.google.gson.Gson;
 import lombok.extern.log4j.Log4j;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -18,6 +21,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {RootConfig.class, ServletConfig.class})
@@ -44,6 +49,20 @@ public class UserDrawTest {
         }
     }
 
+    @Test
+    public void userActionTest(){
+        UserDrawInfo info = new UserDrawInfo();
+        info.setUsername("person1");
+        info.setAmount(2);
+        String json = new Gson().toJson(info);
+        try {
+            mockMvc.perform(post("/draw")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void test1() throws InterruptedException {
